9aeb891841ea1e841365df415d0ee5fff9341e45
上杉颯大 <uesouta0929@gmail.com>
2024-01-23 19:40:55 +0900

    データベースからアプリへデータ転送:後ちょっと

--- yasainikki/app/src/main/java/com/example/yasainikki/bloghyouji.java
+++ yasainikki/app/src/main/java/com/example/yasainikki/bloghyouji.java
@@ -4,18 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class bloghyouji extends AppCompatActivity {
 
+    private TextView textViewPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloghyouji);
 
+        String url = "http://172.21.39.140/courseApp/myblog.php";
+
         RequestQueue queue = Volley.newRequestQueue(this);
+
+        textViewPost = findViewById(R.id.textViewPost);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // レスポンスを処理する
+                        // ここで取得したデータをUIに表示する処理を書く
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // エラーを処理する
+                        // 例: Toastを表示するなど
+                    }
+                });
+
+        // リクエストをRequestQueueに追加する
+        queue.add(jsonObjectRequest);
+
+        new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    // JSONデータから必要な情報を抽出
+                    String postContent = response.getString("post_text");
+
+                    // テキストビューに内容をセット
+                    textViewPost.setText(postContent);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        //RequestQueue queue = Volley.newRequestQueue(this);
     }
 
     public void thirteenth_button1(View view){ finish(); }
